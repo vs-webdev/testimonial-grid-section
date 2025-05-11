b8b226affd083deeffd7eab09fc7c50b9989c91d
Vikas Solanki <solankivikas066@gmail.com>
2025-05-11 19:07:42 +0530

    Replace placeholder content with testimonials and graduate info

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        <div className='daniel-clifford'>
+          <header>
+            <img className='profile-img' src="/image-daniel.jpg" alt="Daniel Clifford" />
+            <div>
+              <p>Daniel Clifford</p>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+            <h3>I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth.</h3>
+            <p>“ I was on EMT for many years before I joined the bootcamp. I've been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it increcibly fun! I enrolled shortly thereafter. The next 12 weeks was the best-and most grueling-time of my life. Since completing the course, I've successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+        </div>
+
+        <div className='jonathan-walters'>
+          <header>
+            <img className='profile-img' src="/image-jonathan.jpg" alt="Jonathan Walters" />
+            <div>
+              <p>Jonathan Walters</p>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+            <h3>The team was very supportive and kept me motivated</h3>
+            <p>“ I started as a total virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I've made in myself. ”</p>
+        </div>
+
+        <div className='kira-whittle'>
+          <header>
+            <img className='profile-img' src="/image-kira.jpg" alt="Kira Whittle" />
+            <div>
+              <p>Kira Whittle</p>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+            <h3>Such a life-changing experience. Highly recommended!</h3>
+            <p>“ Before joining the bootcamp, I've never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and never had to wait long for assistance. The agile team project, in particuler, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I've often referred to it during interviews as an example of my development experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers, 100% recommend! ”</p>
+        </div>
+
+        <div className='jeanette-harmon'>
+          <header>
+            <img className='profile-img' src="/image-jeanette.jpg" alt="Jeanette Harmon" />
+            <div>
+              <p>Jeanette Harmon</p>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+            <h3>An overall wonderful and rewarding experince</h3>
+            <p>“ Thank you for the wonderful experience! I now have a job I really enjoy, and make good living while doing something I love. ”</p>
+        </div>
+
+        <div className='patrick-abrams'>
+          <header>
+            <img className='profile-img' src="/image-patrick.jpg" alt="Patrick Abrams" />
+            <div>
+              <p>Patrick Abrams</p>
+              <p>Verified Graduate</p>
+            </div>
+          </header>
+            <h3>Awesome teaching support from TAs who did the bootcamp themselves.</h3>
+            <p>“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”</p>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
